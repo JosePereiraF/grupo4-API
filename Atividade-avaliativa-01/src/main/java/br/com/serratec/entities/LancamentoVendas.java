b7185bbd78cbe791c4c95a1086f7a91d456ca212
JosePereiraF <jose222125santos@gmail.com>
2024-05-16 19:04:55 -0300

    add projeto

---
+++ Atividade-avaliativa-01/src/main/java/br/com/serratec/entities/LancamentoVendas.java
@@ -0,0 +1,63 @@
+package br.com.serratec.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class LancamentoVendas {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigoVenda;
+	private LocalDate dataVenda;
+	private Double valorVenda;
+	//Relacionamento: Um vendedor possui um ou vários lançamento de vendas.
+	@ManyToOne
+	@JoinColumn(name = "id_vendedor")
+	private Vendedor vendenddor;
+	
+	
+	
+	public LancamentoVendas(Long codigoVenda, LocalDate dataVenda, Double valorVenda, Vendedor vendenddor) {
+		super();
+		this.codigoVenda = codigoVenda;
+		this.dataVenda = dataVenda;
+		this.valorVenda = valorVenda;
+		this.vendenddor = vendenddor;
+	}
+	public LancamentoVendas() {
+		super();
+	}
+	
+	public Long getCodigoVenda() {
+		return codigoVenda;
+	}
+	public void setCodigoVenda(Long codigoVenda) {
+		this.codigoVenda = codigoVenda;
+	}
+	public LocalDate getDataVenda() {
+		return dataVenda;
+	}
+	public void setDataVenda(LocalDate dataVenda) {
+		this.dataVenda = dataVenda;
+	}
+	public Double getValorVenda() {
+		return valorVenda;
+	}
+	public void setValorVenda(Double valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+	public Vendedor getVendenddor() {
+		return vendenddor;
+	}
+	public void setVendenddor(Vendedor vendenddor) {
+		this.vendenddor = vendenddor;
+	}
+	
+	
+}
---
+++ Atividade-avaliativa-01/src/main/java/br/com/serratec/entities/Vendedor.java
@@ -0,0 +1,77 @@
+package br.com.serratec.entities;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Vendedor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	private String nome;
+	@Email
+	private String email;
+	private Double salario;
+	private Double comissao;
+	//Relacionamento: Um vendedor possui um ou vários lançamento de vendas.
+	@OneToMany(mappedBy = "vendedor")
+	private List<LancamentoVendas> listaVendas;
+	
+	
+	public Vendedor() {
+		super();
+	}
+	public Vendedor(Long id, String nome, String email, Double salario, Double comissao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.salario = salario;
+		this.comissao = comissao;
+	}
+	
+	
+	public List<LancamentoVendas> getListaVendas() {
+		return listaVendas;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Double getSalario() {
+		return salario;
+	}
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	public Double getComissao() {
+		return comissao;
+	}
+	public void setComissao(Double comissao) {
+		this.comissao = comissao;
+	}
+	
+	
+}
